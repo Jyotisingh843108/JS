ee3e322968dda082706f3aa26f9e5110b0afac73
Jyotisingh843108 <jyoti843108singh@gmail.com>
2026-05-24 15:53:06 +0530

    practice code

---
+++ JS/app.js
@@ -0,0 +1,98 @@
+
+// console.log("hello world!")
+// console.log("Jyoti singh")
+// let a=10;
+// let b=5;
+// console.log("sum is:", a+b);
+
+// let color = "red";
+// //Traffic Light//
+// if (color == "red"){
+//     console.log("stop! light color is red")
+// }
+
+// if (color == "yellow"){
+//     console.log("slow down. light color is yellow")
+// }
+
+// if (color == "green") {
+//     console.log("go. light color is green")
+// }
+
+let size = "xl"
+
+if (size=="xl") {
+    console.log("price is Rs.250")
+}
+else if (size=="l") {
+    console.log("price is Rs 200")
+}
+
+else if (size=="m") {
+    console.log("price is ERs.100")
+
+
+
+
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+}
+
+else if (size=="s") {
+    console.log("price is Rs.50")
+}
+
+else {
+    console.log("popcorn is for free")
+}
