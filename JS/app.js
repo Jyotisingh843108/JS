342886c81a7fac1b8accc1562693ed1f5f3b3f40
Jyotisingh843108 <jyoti843108singh@gmail.com>
2026-06-04 19:06:20 +0530

    part 3 Js assignment

--- JS/app.js
+++ JS/app.js
@@ -35,7 +35,138 @@
 
 
 
-     
+//============== Practice QUESTIONS ==============================
+
+// let str = "apple"
+// if (str[0] === "a" && str.length>3)
+// {
+//     console.log("good string")
+// }
+// else{
+//     console.log("not a good string")
+// }
+
+
+// let num = 12;
+
+// if((num%3===0) && ((num+1===15)|| (num-1 ===11)))
+// {
+//     console.log("safe")
+// }
+// else{
+//     console.log("unsafe")
+// }
+
+
+//Assignment part 2//
+
+// let num = 20;
+//  if (num%10==0)
+//  {
+//     console.log("good");
+//  }
+//  else 
+//  {
+//     console.log(bad);
+//  }
+
+// let name = prompt("enter your name");
+// let age = prompt("enter your age");
+// console.log(name , age)
+
+
+// let quater = 5;
+
+// switch (quater){
+// case 1:
+//     console.log("January, FEB, March");
+//     break;
+// case 2:
+//     console.log("April, May, June");
+//     break;
+// case 3:
+//     console.log("July, August, September");
+//     break;
+// case 4: 
+//     console.log("October,November,December")
+//     break;
+
+// default:
+//     console.log("Wrong quarter selected");
+// }
+
+
+// 
+
+// let a = 5;
+// let b = 8;
+// let c = 10;
+
+// if (a>b)
+// {
+//     if(a>c)
+//     {
+//         console.log(a);
+//     }
+//     else
+//     {
+//         console.log(c);
+//     }
+// }
+// else{ // b > a
+//     if(b>c)
+//     {
+//         console.log(b);
+
+//     }
+//     else
+//     {
+//         console.log(c);
+//     }
+// }
+   
+
+
+// 4, 5, 6
+
+//if 4 > 5
+  
+   //if 4 > 6
+  // else 6 > 4
+  
+//else 5 > 4
+
+  //if 6>5 (answer will be 6)
+  //else 5>6
+
+
+
+
+
+  ////===============================////
+
+
+//  let a = 10
+//  let b = 8
+//  let c = 15
+
+// if (a > b) {
+//     if (a > c) {
+//         console.log("a is largest num")
+//     }
+//     else {
+//         console.log("c, is largest num")
+//     }
+// }
+
+// else {
+//     if (b > c) {
+//         console.log("b is greater num")
+//     }
+//     else {
+//         console.log("c, graeter number")
+//     }
+// }
 
 
 
@@ -44,34 +175,15 @@
 
 
 
+// let num1 = 32;
+// let num2 = 47852;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// if ((num1%10) == (num2%10)) {
+//     console.log("number have same last digit 2")
+// }
+// else {
+//     console.log("numbers do not have the same last digit")
+// }
 
 
 
@@ -87,12 +199,12 @@
 
 
 
-}
+// // }
 
-else if (size=="s") {
-    console.log("price is Rs.50")
-}
+// // else if (size=="s") {
+// //     console.log("price is Rs.50")
+// // }
 
-else {
-    console.log("popcorn is for free")
-}
+// // else {
+// //     console.log("popcorn is for free")
+// // }
---
+++ part_3_app.js
@@ -0,0 +1,56 @@
+// let start = ["january","july","march","august"]
+
+// QUESTION 1
+
+// let number = [7,9,0,-2];
+// let n = 3;
+
+// let ans = number.slice(0,n);
+// console.log(ans);
+
+//Question 2//
+
+// let array = [7,9,0,-2];
+// let n=3;
+
+// //let ans = array.slice(array.length-n,array.length);
+
+// //let ans = array.slice(array.length-n);
+
+// let ans = array.slice(-n);
+
+
+//Question3//
+
+// let string = "blank";
+  
+// if(string.length===0)
+// {
+//     console.log("string is emplty")
+// }
+// else{
+//     console.log("string is not empty")
+// }
+
+//Question 4//
+
+// 
+
+//question 5 //
+
+// let string = "    apnacollage";
+// console.log(`original string = ${string}`);
+// console.log(`string without string= ${string.trim()}`)
+
+//Question 6//
+
+let array = [20,65,55,98,100];
+let n=120;
+
+if(array.indexOf(n) !==-1)
+{
+    console.log("found")
+}
+else{
+    console.log("not found")
+}
